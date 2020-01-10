0d1fb53ed1f57d515c16bfcfb856747911386a75
MissionLee <missionlee@163.com>
2020-01-10 21:26:15 +0800

    v0.1

--- test/test2.js
+++ test/test2.js
@@ -1,6 +1,6 @@
 // js 中使用 import xxx from yyy 的时候，会报错 SyntaxError: Unexpected token xxx
 // 使用require的时候，无法直接取到默认值，还要在后面加上一个 .default 我们这边的处理方法是在 index2.js 里面转接的时候输出default
-let CacheController =require("../index2")
+let CacheController =require("../index")
 CacheController.setCache('a',"hello")
 CacheController.getCache('a').then(function(value){
     console.log(value)
